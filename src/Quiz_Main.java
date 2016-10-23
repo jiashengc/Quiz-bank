da63e8c3df4aeae7df0bf26d4694cf3cc3a3c9bb
Chong Jia Sheng <jiashengc@hotmail.com>
2016-10-23 21:11:06 +0800

    Add files via upload

--- src/Quiz_Main.java
+++ src/Quiz_Main.java
@@ -4,6 +4,46 @@ public class Quiz_Main {
 	public static void main(String[] args) {
 		
 		Scanner input = new Scanner(System.in);
+		int userInput = 0;
+		
+		System.out.println("Welcome to House Mormont's Quiz Bank!");
+		System.out.println("Take a look at the options and choose whichever!");
+		
+		menu(input);
+		
+		
+	}
+	
+	public static void menu(Scanner input) {
+		
+		int userInput = 0;
+
+		System.out.println("");
+		System.out.println("Options \t Input");
+		System.out.println("========================");
+		System.out.println("Quizes \t\t 1");
+		System.out.println("Results \t 2");
+		System.out.println("========================");
+		System.out.println("Input -1 to EXIT.\n");
+		System.out.print("Input: ");
+		userInput = input.nextInt();
+		System.out.println("");
+		
+		switch(userInput) {
+			case -1:
+				System.out.println("Goodbye! See you again!");
+				break;
+			case 1:
+				System.out.println("Quizes");
+				break;
+			case 2: 
+				System.out.println("Results");
+				break;
+			default:
+				System.out.println("Invalid Input!");
+				System.out.println("Please re-enter!");
+				menu(input);
+		}
 		
 	}
 }
